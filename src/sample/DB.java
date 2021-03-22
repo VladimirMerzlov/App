688dc5770a8b6c3ae04f84e6851176c2afc1bff6
Merzlov V.V <vladimir_merzlov@mail.ru>
2021-03-22 21:55:44 +0300

    Added db and some styles

---
+++ src/sample/DB.java
@@ -0,0 +1,32 @@
+package sample;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DB {
+
+    private final String HOST = "localhost";
+    private final String PORT = "8889";
+    private final String DB_NAME = "app_db";
+    private final String LOGIN = "root";
+    private final String PASS = "root";
+
+    private Connection dbConn = null;
+
+    private Connection getDbConnection() throws ClassNotFoundException, SQLException {
+        String connStr = "jdbc:mysql://" + HOST + ":" + PORT + "/" + DB_NAME;
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        dbConn = DriverManager.getConnection(connStr, LOGIN, PASS);
+        return dbConn;
+
+    }
+
+    public void isConnected () throws SQLException, ClassNotFoundException {
+        dbConn = getDbConnection();
+        System.out.println(dbConn.isValid(1000));
+
+    }
+
+}
